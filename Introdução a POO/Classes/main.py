4f2b63b116fe985ff83834043b15e3b28921a7bb
Thiago Oliveira Costa Diniz <thiagoocdiniz@gmail.com>
2022-11-06 17:35:27 -0300

    Classes

--- Introdução a POO/Classes/main.py
+++ Introdução a POO/Classes/main.py
@@ -1,6 +1,10 @@
 from pessoa import Pessoa
 
-p1 = Pessoa()
-p2 = Pessoa()
+p1 = Pessoa('Thiago', 33)
+p2 = Pessoa('Luiz', 29)
 
-p1.falar()
+p1.comer('manga')
+p1.parar_comer()
+p1.parar_comer()
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
