563c73d6ffd94fd9bea5e4151eda526cb8b9e285
Thiago Oliveira Costa Diniz <thiagoocdiniz@gmail.com>
2022-12-11 14:34:23 -0300

    Mixins

---
+++ Introdução a POO/Mixins/main.py
@@ -0,0 +1,8 @@
+from log import LogFileMixin, LogPrintMixin
+from eletronico import Smartphone
+
+galaxy_s = Smartphone('Galaxy S22')
+iphone = Smartphone('Iphone 14 Pro')
+
+galaxy_s.ligar()
+iphone.desligar()
