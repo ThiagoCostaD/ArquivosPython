d88ceaa88a2f8d696d8e3fd61bd7c3425d3a0217
Thiago Oliveira Costa Diniz <thiagoocdiniz@gmail.com>
2023-01-04 19:19:13 -0300

    ExerccicioContas

--- Introdução a POO/ExercicioBanco/contas.py
+++ Introdução a POO/ExercicioBanco/contas.py
@@ -2,20 +2,21 @@ import abc
 
 
 class Conta(abc.ABC):
-    def __init__(self, agencia, conta, saldo):
+    def __init__(self, agencia: int, conta: int, saldo: float = 0) -> None:
         self.agencia = agencia
         self.conta = conta
         self.saldo = saldo
 
     @abc.abstractmethod
-    def sacar(self, valor): ...
+    def sacar(self, valor: float) -> float: ...
 
-    def depositar(self, valor):
-        self.valor += valor
-        self.detalhes(f'(DEPÓSITO {valor}')
+    def depositar(self, valor: float) -> float:
+        self.saldo += valor
+        self.detalhes(f'(DEPÓSITO {valor})')
 
-    def detalhes(self, msg=''):
-        print(f'O seu saldo é{self.saldo:.2f} {msg}')
+    def detalhes(self, msg: str = '') -> None:
+        print(f'O seu saldo é {self.saldo:.2f} {msg}')
+        print('--')
 
 
 class ContaPoupanca(Conta):
@@ -24,12 +25,40 @@ class ContaPoupanca(Conta):
 
         if valor_pos_saque >= 0:
             self.saldo -= valor
-            self.detalhes(f'(SAQUE {valor}')
+            self.detalhes(f'(SAQUE {valor})')
             return self.saldo
 
-    print('Não foi possível sacar o valor desejado')
-    self.detalhes(f'(SAQUE {valor}')
+        print('Não foi possível sacar o valor desejado')
+        self.detalhes(f'(SAQUE NEGADO {valor})')
+
+
+class ContaCorrente(Conta):
+    def __init__(
+            self, agencia: int, conta: int, saldo: float = 0,
+            limite: float = 0):
+        super().__init__(agencia, conta, saldo)
+        self.limite = limite
+
+    def sacar(self, valor: float) -> float:
+        valor_pos_saque = self.saldo - valor
+        limite_maximo = -self.limite
+
+        if valor_pos_saque >= limite_maximo:
+            self.saldo -= valor
+            self.detalhes(f'(SAQUE {valor})')
+            return self.saldo
+
+        print('Não foi possível sacar o valor desejado')
+        self.detalhes(f'(SAQUE NEGADO {valor})')
 
 
 if __name__ == '__main__':
-    ...
+    cp1 = ContaPoupanca(111, 222)
+    cp1.sacar(1)
+    cp1.depositar(10)
+    cp1.sacar(2)
+    print('-'*80)
+    cc1 = ContaCorrente(101, 202, 0, 100)
+    cc1.sacar(1)
+    cc1.depositar(10)
+    cc1.sacar(2)
